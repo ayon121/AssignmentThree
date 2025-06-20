dc457860f6de607faebe30a29b760375cac780b6
ayon121 <borshonsaha1234@gmail.com>
2025-06-20 20:31:57 +0600

    book get with filtering added

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -3,21 +3,52 @@ import { Book } from "../models/book.model"
 
 export const bookRoutes = express.Router()
 
+
+// creating book controller
 bookRoutes.post("/", async (req: Request, res: Response) => {
     try {
         const body = req.body
         const book = await Book.create(body)
-        res.status(201).json({
+        res.status(200).json({
             "success": true,
             "message": "Book created successfully",
             "data": book,
         });
     } catch (error: any) {
         res.status(500).json({
-            message : "Validation failed",
-            success : false ,
+            message: "Validation failed",
+            success: false,
             error: error,
         })
 
     }
 })
+
+
+// getting all the book controller
+bookRoutes.get("/", async (req: Request, res: Response) => {
+    try {
+        const { filter,
+            sortBy,
+            sort,
+            limit = "10"
+        } = req.query 
+
+        const sortOrder = sort === "asc" ? 1 : -1;
+
+        const books = await Book.find({genre : filter}).sort({ [sortBy as string]: sortOrder }).limit(parseInt(limit as string))
+        res.status(200).json({
+            success: true,
+            message: "Books retrieved successfully",
+            data: books
+        });
+
+    } catch (error: any) {
+        res.status(500).json({
+            message: "Validation failed",
+            success: false,
+            error: error,
+        })
+    }
+})
+
