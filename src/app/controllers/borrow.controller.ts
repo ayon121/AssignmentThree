a047266da118352a0b9617b6de00bed09903a200
ayon121 <borshonsaha1234@gmail.com>
2025-06-21 17:12:55 +0600

    Borrowed book showing added

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express"
 import { Book } from "../models/book.model";
 import { Borrow } from "../models/borrow.model";
+import mongoose from "mongoose";
+
 
 
 
@@ -28,4 +30,70 @@ borrowRoutes.post("/", async (req: Request, res: Response) => {
         })
     }
 
+})
+
+
+
+// borrow book aggregation 
+borrowRoutes.get("/", async (req, res) => {
+    try {
+        const summary = await Borrow.aggregate([
+            // pipeline 1 - converting bookid
+            {
+
+                $addFields: {
+                    bookObjectId: { $toObjectId: "$book" }
+                }
+
+            },
+
+            // pipeline 2  grouping by bookid
+            {
+                $group: {
+                    _id: "$bookObjectId",
+                    totalQuantity: { $sum: "$quantity" }
+                }
+            },
+
+            // pipeline 3 - macthing tables
+            {
+                $lookup: {
+                    from: "books",
+                    localField: "_id",
+                    foreignField: "_id",
+                    as: "bookInfo"
+                }
+            },
+
+            // pipeline 4 - unwinding bookinfo from previous pipeline
+            {
+                $unwind: "$bookInfo"
+            },
+
+            // pipeline 5 - projecting required fields
+            {
+                $project: {
+                    _id: 0,
+                    book: {
+                        title: "$bookInfo.title",
+                        isbn: "$bookInfo.isbn"
+                    },
+                    totalQuantity: 1
+                }
+            }
+        ])
+        res.status(200).json({
+            success: true,
+            message: "Borrowed books summary retrieved successfully",
+            data: summary
+        });
+
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Validation failed",
+            success: false,
+            error: error,
+        })
+    }
 })
